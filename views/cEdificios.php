9f2bcd797fab11c0bd1fe66e42e47ee34cc88a04
AlfredoVazquezMaldonado <alvazquezma@ittepic.edu.mx>
2016-11-28 18:44:14 -0700

    Edificios

---
+++ views/cEdificios.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="../js/plugins/jquery-3.1.0.min.js"></script>
+        <script type="text/javascript" src="../js/plugins/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" href="../css/bootstrap.min.css" media="screen">
+    </head>
+    <body>
+    <div ><br></div>
+    <div ><br></div>
+    <div ><br></div>
+    <div ><br></div>
+    <div ><br></div>
+
+        <div class="container">
+            <div class="row">
+                    <div class="col col-lg-8 col-md-8 col-sm-8 col-xs-8 col-sm-offset-2">
+                        <form id="frmRole">
+                            <div class="form-group">
+                                <label class="control-label" for="edificioname"></label>
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="glyphicon glyphicon-new-window"></i>
+                                    </span>
+                                    <input class="form-control " id="edificioname" name="edificioname" placeholder="Nombre del Edifico">
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label class="control-label" for="edificioabreviacion"></label>
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="glyphicon glyphicon-flash"></i>
+                                    </span>
+                                    <input class="form-control " id="edificioabreviacion" name="edificioabreviacion" placeholder="Abreviación">
+                                </div>
+                            </div>
+                            
+                            <div align="right">
+                                <button type="submit" class="btn btn-primary btn-sm">
+                                    <i class="glyphicon glyphicon-save-file"></i>Guardar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                </div>
+        </div>
+    </body>
+</html>
