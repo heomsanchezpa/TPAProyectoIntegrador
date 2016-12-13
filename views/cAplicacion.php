91cf1cbfef2683b585b4a0a99874246ce08499c8
zomi3 <zomi3.ts@gmail.com>
2016-12-13 16:42:15 -0700

    Aplicación Encuesta

---
+++ views/cAplicacion.php
@@ -0,0 +1,13 @@
+<script type="text/javascript" src="js/caplicacion.js"></script>
+
+
+<div class="row">
+    <div class="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <form id="frmAplicacion" method="POST">
+        	<div class="form-group">                        
+                <h1>Aplicacion de  <span class="label label-default"> Encuesta
+                </span></h1>
+         	</div>                                  
+        </form>
+    </div>
+</div>
