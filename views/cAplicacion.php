f9f069d3cb08fa51ed9d52142334dfdc74e5e5e7
zomi3 <zomi3.ts@gmail.com>
2016-12-15 11:22:18 -0700

    Update cAplicacion.php

--- views/cAplicacion.php
+++ views/cAplicacion.php
@@ -3,11 +3,21 @@
 
 <div class="row">
     <div class="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <form id="frmAplicacion" method="POST">
+        <form id="frmAplicacion" method="POST" >
         	<div class="form-group">                        
-                <h1>Aplicacion de  <span class="label label-default"> Encuesta
+                <h1 id="titulo1">Aplicacion de  <span class="label label-default"> Encuesta
                 </span></h1>
-         	</div>                                  
+         	</div> 
+         </form>   
+         <form id="frmAplicacion2" hidden="true">        
+         <h1>Encuesta ya <span class="label label-danger"> Realizada</span></h1>
+            <div class="row">
+                    <div id="content" class="col-lg-12">                   
+                        <div align="center">
+                            <img src="/../TPAProyectoIntegrador/image/gracias.jpg" class="img-responsive" alt="">
+                        </div>
+                    </div>
+                </div>                                                      
         </form>
     </div>
 </div>
